e864c97d2e9c1b35823ed0535c4efc53a5b6cdb5
TallonsCZ <tallons@burningcube.eu>
2024-12-04 11:38:40 +0100

    Adds Holograms

--- src/main/java/cz/tallonscz/upgradablespawner/Utilities/Holograms.java
+++ src/main/java/cz/tallonscz/upgradablespawner/Utilities/Holograms.java
@@ -1,14 +1,35 @@
 package cz.tallonscz.upgradablespawner.Utilities;
 
 import org.bukkit.Location;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Entity;
 
 public class Holograms {
 
-    public void createHologram(Location location){
+    public void createHologram(Location location, String... inLines){
+        String[] lines;
+        lines = inLines;
 
+
+        for (String line : lines){
+            ArmorStand stand = location.getWorld().spawn(location, ArmorStand.class);
+
+            stand.setVisible(false);
+            stand.setGravity(false);
+            stand.setInvulnerable(false);
+
+            stand.setCustomNameVisible(true);
+            stand.setCustomName(Common.colorize(line));
+
+            location.subtract(0,0.25, 0);
+        }
     }
 
     public void deleteHologram(Location location){
-
+        for(Entity entity: location.getWorld().getNearbyEntities(location, 2, 5, 2)){
+            if(entity instanceof ArmorStand && !entity.hasGravity()){
+                entity.remove();
+            }
+        }
     }
 }
