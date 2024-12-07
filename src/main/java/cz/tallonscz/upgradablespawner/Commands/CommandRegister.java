5f11617cb4156e724ab6e9fe26e8cdd8d5325acb
TallonsCZ <tallons@burningcube.eu>
2024-12-07 22:44:06 +0100

    Optimazite imports

--- src/main/java/cz/tallonscz/upgradablespawner/Commands/CommandRegister.java
+++ src/main/java/cz/tallonscz/upgradablespawner/Commands/CommandRegister.java
@@ -1,9 +1,6 @@
 package cz.tallonscz.upgradablespawner.Commands;
 
 import cz.tallonscz.upgradablespawner.Upgradablespawner;
-import org.bukkit.Server;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.PluginManager;
 
 public class CommandRegister {
 
